75165dc5d6a82e0a33c3098ad81f40b239689b6a
Tiago Botelho <tiagonbotelho@hotmail.com>
2015-02-09 21:09:56 +0000

    added init file

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'date'
+require 'slack-post'
+
+
+Slack::Post.configure(
+  subdomain: 'myslack',
+  token: 'xoxp-3666247738-3666247752-3664405749-809162',
+  username: 'tiagonbotelho'
+)
+Slack::Post.post 'Ola o meu nome e tiago', '#birthday_test'
